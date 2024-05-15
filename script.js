23f2f309745aaa64f728beed158bd06adcc3f640
Maria <mmaferrrr@outlook.com>
2024-05-15 12:50:36 -0400

    Updated file

--- script.js
+++ script.js
@@ -46,21 +46,28 @@ function validateContactForm(event) {
 
 // Fetching JSON file
 
+//Initialize fetch to the contents of products.json.
     fetch('products.json')
+    
+        //Waits for the response from the server and converts it into an object using the.json method.
+        //receives the parsed JSON data once the conversion is complete.
         .then(response => response.json())
         .then(data => {
             let productContainer = document.querySelector(".productContainer");
 
+
+        // For each product, it creates a new div (container) to hold the product's details.
+        //Then it sets up various elements (image, title,price, description and button).
             data.forEach(product => {
                 const productDiv = document.createElement('div');
-                productDiv.classList.add('product-div');
+                productDiv.classList.add('product-div'); // classList.add adds a class to the element for styling purposes.
 
                 const productImg = document.createElement('img');
                 productImg.src = product.image;
                 productImg.classList.add('product-image');
 
                 const productTitle = document.createElement('h2');
-                productTitle.textContent = product.title;
+                productTitle.textContent = product.title; // Sets the text content of an element.
     
                 const productPrice = document.createElement('p');
                 productPrice.textContent = product.price;
@@ -71,8 +78,9 @@ function validateContactForm(event) {
                 const productButton = document.createElement('button');
                 productButton.textContent = product.button;
                 
-
-                productDiv.appendChild(productImg);
+                //Appends a child element to the parent element.
+                //Each new div is appended to the productContainer found in the html.
+                productDiv.appendChild(productImg); 
                 productDiv.appendChild(productTitle);
                 productDiv.appendChild(productPrice);
                 productDiv.appendChild(productDescription);
